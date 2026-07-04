d3e7e5e5c3d9c4be1837a3feb14d822a057647eb
IsraelJr <32980364+IsraelJr@users.noreply.github.com>
2026-07-04 00:34:05 -0300

    Fix title in metadata for layout component

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,7 +2,7 @@ import type { Metadata } from "next";
 import "./globals.css";
 
 export const metadata: Metadata = {
-  title: "Serenity at Dusk do Léo",
+  title: "Serenity at of Léo",
   description: "Visual novel infantil sobre escolhas financeiras simples.",
 };
 
